6e1f1f1c9d8139ee85ac250cba11fb75a35abdc4
phdaccache <pedrodaccache@gmail.com>
2023-06-19 22:00:43 -0300

    .

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,6 @@ import menu.MenuPadrao;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        /******************************* MENU INTERATIVO *******************************/
-
         Scanner scanner = new Scanner(System.in);        
         MenuPadrao menu = new MenuPadrao(scanner);
         menu.runMenu();
