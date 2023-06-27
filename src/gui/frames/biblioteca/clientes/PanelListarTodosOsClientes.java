a2336280943cc4aab6076bdae61d2fccb5e25aba
phdaccache <pedrodaccache@gmail.com>
2023-06-26 21:40:59 -0300

    inputs e simulação terminal

--- src/gui/frames/biblioteca/clientes/PanelListarTodosOsClientes.java
+++ src/gui/frames/biblioteca/clientes/PanelListarTodosOsClientes.java
@@ -60,5 +60,21 @@ public class PanelListarTodosOsClientes extends JPanel {
 			}
 		});
 		add(iconX);
+		
+		// Simulacao do terminal 
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 125, 326, 230);
+		add(scrollPane);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setSelectionColor(MyColors.ACCENT);
+		textArea.setEditable(false);
+		textArea.setMargin(new Insets(10, 10, 10, 10));
+		textArea.setFont(new Font("Arial", Font.PLAIN, 12));
+		scrollPane.setViewportView(textArea);
+		
+		// String recebida do backend
+		String string = "teste\nteste";
+		textArea.setText(string);
 	}
 }
