27f6b6b186cafa26dfb31e253450bc59284d5155
phdaccache <pedrodaccache@gmail.com>
2023-06-30 15:39:36 -0300

    clean main

--- src/run/Main.java
+++ src/run/Main.java
@@ -25,13 +25,6 @@ public class Main {
 				e.printStackTrace();
 			}
 		}));
-
-		// ArrayList<Biblioteca> listaBiblioteca = new ArrayList<Biblioteca>();
-		// Biblioteca b1 = new Biblioteca("Biblioteca", "81277798000115", "Rua 11", "(00) 00000-0000", null, null, null, null, "123");
-		// Biblioteca b2 = new Biblioteca("Bibliotecaa", "22475123000111", "Rua 11", "(00) 00000-0000", null, null, null, null, "123");
-		// listaBiblioteca.add(b1);
-		// listaBiblioteca.add(b2);
-		// System.out.println(GravarDadosBiblioteca(listaBiblioteca));
 	}
     
     public Main() {
