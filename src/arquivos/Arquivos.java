5f251bdf7fbad9b8c46453422fbf3146e9d27c8a
phdaccache <pedrodaccache@gmail.com>
2023-06-30 15:42:16 -0300

    new pacote

--- src/arquivos/Arquivos.java
+++ src/arquivos/Arquivos.java
@@ -1,10 +1,12 @@
-package sistema;
+package arquivos;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+
+import sistema.Biblioteca;
 
 public class Arquivos {
 
